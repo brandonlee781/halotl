2472226239d0817fb0f515ecc55152608dfaff58
Brandon Lee <brandonlee781@gmail.com>
2022-05-05 16:13:51 -0500

    Was messing up mobile

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,7 @@
 
 <template>
   <header>
-    <div class="wrapper text-[#4e78ba] mx-auto w-[540px] text-center">
+    <div class="wrapper text-[#4e78ba] mx-auto max-w-[540px] text-center">
       <h1 class="text-4xl font-bold">HALOTL</h1>
       <h2 class="text-2xl mt-2">HCS PLAYER GUESSING GAME</h2>
     </div>
